b1b63b670915fee06cc9a9307b759f7021cb4a64
astuciasnor <astuciasnor@gmail.com>
2024-10-23 18:56:45 -0300

    apagando, modificando e criando arquivos

---
+++ request.R
@@ -0,0 +1,24 @@
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+#
+#                      PRATICANDO O PULL REQUEST
+#
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+# Crie um vetor x numérico aqui -----------------------------------------------
+
+
+
+# Crie um vetor y em função do vetor x acima ----------------------------------
+
+
+
+# Crie um dataframe chamado df unindo os vetores x e y ------------------------
+
+
+
+# Crie um grafico de pontos com o dataframe df o ggplot -----------------------
+
+
+
+
+# Adicione ao gráfico ma curva de tendência suavizada(smooth) -----------------

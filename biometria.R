a68d02ba36b916fdf270fe6d41bdcc73eb08fe01
astuciasnor <astuciasnor@gmail.com>
2024-10-24 21:43:16 -0300

    Mudei o biometria

--- biometria.R
+++ biometria.R
@@ -1,37 +1,31 @@
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 #
-#             Criação de gráfico de Barras lado a lado ou Empilhado
+#             Criação de gráfico de dispersão de pontos
 #
 #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
 library(ggplot2)
 library(readr)
 
-# Importando dados de biometria
+# Importando dados de biometria ------------------------------------------------
 dados <- read_csv("dados/biometria.csv")
 
 # Visualizando o conjunto de dados 
 print(dados)
 
-# Criação do gráfico de barras lado a lado -------------------------------
-ggplot(dados, aes(x = `Comprimento (cm)`, y = `Peso (g)`))
+# Crie a base do grafico usando ggplot2  ---------------------------------------
+dados |> 
+  ggplot(aes(x = `Comprimento (cm)`, y = `Peso (g)`))
 
-# Criação do gráfico de barras empilhadas ------------------------------
-ggplot(dados, aes(x = Category, y = Value, fill = Group)) +
-  geom_bar(stat = "identity", position = "stack") +
-  labs(title = "Gráfico de Barras Empilhadas",
-       x = "Categoria",
-       y = "Valor") +
+# Acrescente a camada de pontos ------------------------------------------------
+dados |> 
+  ggplot(aes(x = `Comprimento (cm)`, y = `Peso (g)`)) +
+  geom_point() 
+
+# Acrescente o título, uma reta e mude o tema ----------------------------------
+dados |> 
+  ggplot(aes(x = `Comprimento (cm)`, y = `Peso (g)`)) +
+  geom_point() +
+  labs(title = "Gráfico de dispersão") +
+  geom_smooth(method = "lm") +
   theme_minimal()
-
-# Mudando a ordem dos grupos e fazendo novo gráfico
-dados$Group <- factor(dados$Group, levels = c("Group3", "Group2", "Group1"))
-
-ggplot(dados, aes(x = Category, y = Value, fill = Group)) +
-  geom_bar(stat = "identity", position = "stack") +
-  labs(title = "Gráfico de Barras Empilhadas",
-       x = "Categoria",
-       y = "Valor") +
-  theme_minimal()
-
 
